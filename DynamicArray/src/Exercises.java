3ce78cfbbf5bdb3b7f3cca00118878ed2e605ba3
Ben Wernicke <ben@wernicke.at>
2025-04-02 22:27:44 +0200

    more stuff

---
+++ DynamicArray/src/Exercises.java
@@ -0,0 +1,9 @@
+public class Exercises {
+    public DynamicArray mergeArrays(int[] a, int[] b)  {
+        return null;
+    }
+
+    public DynamicArray fibonaccinumbers(int[] a, int[] b) {
+        return null;
+    }
+}
